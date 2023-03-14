02ffa6bdd452d40fa567b0f9a3067778f6346b9b
Gabriela1001 <neagugabriela20@stud.ase.ro>
2023-03-15 00:19:33 +0200

    should be fine

--- src/cts/s02/principii_clean_code/clase/Angajat.java
+++ src/cts/s02/principii_clean_code/clase/Angajat.java
@@ -6,6 +6,17 @@ public class Angajat extends Aplicant {
     private String ocupatie;
     private int salariu;
 
+    public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumire_Proiecte, int salariu, String ocupatie) {
+        super(nume, prenume, varsta, punctaj, nr_proiecte, denumire_Proiecte);
+        this.salariu = salariu;
+        this.ocupatie = ocupatie;
+    }
+
+    public Angajat() {
+        super();
+
+    }
+
     public String getOcupatie() {
         return ocupatie;
     }
@@ -21,18 +32,6 @@ public class Angajat extends Aplicant {
     public void setSalariu(int salariu) {
         this.salariu = salariu;
     }
-
-    public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumire_Proiecte, int salariu, String ocupatie) {
-        super(nume, prenume, varsta, punctaj, nr_proiecte, denumire_Proiecte);
-        this.salariu = salariu;
-        this.ocupatie = ocupatie;
-    }
-
-    public Angajat() {
-        super();
-
-    }
-
 
     @Override
     public String toString() {
--- src/cts/s02/principii_clean_code/clase/readers/AplicantReader.java
+++ src/cts/s02/principii_clean_code/clase/readers/AplicantReader.java
@@ -1,11 +1,11 @@
 package cts.s02.principii_clean_code.clase.readers;
 
+import cts.s02.principii_clean_code.clase.Aplicant;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import cts.s02.principii_clean_code.clase.Aplicant;
-
 
 public abstract class AplicantReader {
-	public abstract List<Aplicant> readAplicants(String file) throws FileNotFoundException;
+    public abstract List<Aplicant> readAplicants(String file) throws FileNotFoundException;
 }
